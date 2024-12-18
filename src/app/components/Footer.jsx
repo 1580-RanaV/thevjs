882510775c741f61bae450859c657f15ac486b59
ranavgh18 <rviyyapu@gitam.in>
2024-12-18 15:34:20 +0530

    footer change

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="text-neutral-600 text-center py-4 font-medium text-xs">
-        © Copyright V Jaya Surya Personal Portfolio 2024. All rights reserved.<br></br>Designed and built by <span className='underline'>Rana</span>
+        © Copyright V Jaya Surya 2024 - 2025. All rights reserved.<br></br>Designed and built by <span className='underline'>Rana</span>
     </footer>
   )
 }
