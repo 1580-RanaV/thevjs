38ab25b721ea8dedf92c4669ead042fe5fc35914
ranavgh18 <rviyyapu@gitam.in>
2024-12-19 09:18:22 +0530

    font sizes

--- src/app/components/ExperienceEntry.jsx
+++ src/app/components/ExperienceEntry.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ExperienceEntry = ({ company, location, position }) => {
   return (
     <div className="mt-6 text-justify leading-relaxed">
-      <span className="font-semibold text-lg md:text-lg">{company}</span>{' '}
+      <span className="font-semibold">{company}</span>{' '}
       <span className="font-regular text-lg">|</span>{' '}
-      <span className="font-regular text-lg">{location}</span>
-      <span className="block text-neutral-400 text-lg md:text-lg">
+      <span className="font-regular">{location}</span>
+      <span className="block text-neutral-400">
         {position}
       </span>
     </div>
